420019951b978363182bfc200f48b6c8a2a8eb63
Gowrisankar <gsankar042@gmail.com>
2021-04-30 17:00:05 +0530

    Update BasePage.java

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
